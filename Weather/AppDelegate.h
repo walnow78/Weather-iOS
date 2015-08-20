2f99a10e0082c3e43fac0402d5b8aee5fd049e40
Pawel Walicki <pawelwalicki@MacBook-Pro-de-Pawel.local>
2015-08-20 15:04:25 +0200

    Start project

---
+++ Weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by Pawel Walicki on 20/8/15.
+//  Copyright (c) 2015 lemurinthebox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weather/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Weather
+//
+//  Created by Pawel Walicki on 20/8/15.
+//  Copyright (c) 2015 lemurinthebox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
